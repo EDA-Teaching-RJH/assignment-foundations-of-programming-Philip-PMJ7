c85cf9e86f255d704d3820855db0f21953fd41f4
Philip-PMJ7 <pmj7@kent.ac.uk>
2026-02-22 17:43:48 +0000

    Part 1: Eighth Error - Logical, Line 49

--- old_system.py
+++ old_system.py
@@ -45,11 +45,14 @@ def run_system_monolith():
         elif opt == "3":
             rem = input("Name to remove: ")
            
-            idx = n.index(rem)
-            n.pop(idx)
-            r.pop(idx)
-            d.pop(idx)
-            print("Removed.")
+            if rem in n:
+                idx = n.index(rem)
+                n.pop(idx)
+                r.pop(idx)
+                d.pop(idx)
+                print("Removed.")
+            else:
+                print("Crew member not found. Returning to view screen.")
             
         elif opt == "4":
             print("Analyzing...")
