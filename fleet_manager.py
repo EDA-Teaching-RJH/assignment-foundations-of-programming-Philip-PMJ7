c92aa2c2fe61748492dd5018adef295f9bee5d6a
Philip-PMJ7 <pmj7@kent.ac.uk>
2026-02-22 19:51:47 +0000

    Part 2: update_rank

--- fleet_manager.py
+++ fleet_manager.py
@@ -12,7 +12,7 @@ def display_menu(names):
     else:
         print(f"Logged in as: {user}")
     
-    print("-- Displaying Menu --\n1. Display Roster\n2. Add Crew\n3. Remove Crew\n4. Search Crew\n5. Exit")
+    print("-- Displaying Menu --\n1. Display Roster\n2. Add Crew\n3. Remove Crew\n4. Update Rank\n5. Exit")
 
     while True:
         try:
@@ -71,7 +71,7 @@ def remove_member(names, ranks, divs, ids):
         try:
             remove_id = int(input("Enter ID of Crew Member to be removed: "))
         except ValueError:
-            print("IDs can only be Integers")
+            print("IDs can only be Integers.")
         else:
             if remove_id in ids:
                 index = ids.index(remove_id)
@@ -88,6 +88,37 @@ def remove_member(names, ranks, divs, ids):
             else:
                 print("No Crew Member exists with that ID. Try again.")
 
+def update_rank(names, ranks, ids):
+    while True:
+        try:
+            update_id = int(input("Enter the ID of the Crew Member whose Rank will be updated."))
+        except ValueError:
+            print("IDs can only be Integers.")
+        else:
+            if update_id in ids:
+                print("ID verified.")
+                break
+            else:
+                print("There is no current Crew Member with that Rank.")
+                continue
+    
+    index = ids.index(update_id)
+    current_name = names[index]
+    current_rank = ranks[index]
+    valid_rank = ["Fleet Admiral", "Admiral", "Captain", "Commander", "Lieutenant Commander", "Lieutenant", "Lieutenant Junior Grade", "Ensign", "Officer", "Cadet"]
+    while True:
+        new_rank = input("Enter new rank: ")
+        if new_rank in valid_rank:
+            ranks[index] = new_rank
+            print(f"{current_name} has been updated from {current_rank} to {new_rank}.")
+            break
+        else:
+            print("That is not a Rank. Try again.")
+    
+    return names, ranks, ids
+    
+
+
 def main():
     names, ranks, divs, ids = init_database() #Sets up lists at the start
     option = display_menu(names) #Receives option from display
@@ -97,5 +128,9 @@ def main():
         names, ranks, divs, ids = add_member(names, ranks, divs, ids)
     elif option == 3:
         names, ranks, divs, ids = remove_member(names, ranks, divs, ids)
+    elif option == 4:
+        names, ranks, ids = update_rank(names, ranks, ids)
+    
+
 
 main()
